5ae57e57d8a17cee3b110c16ff91636db316a8a2
asmaAouadi <133607311+asmaAouadi@users.noreply.github.com>
2024-01-13 12:06:36 +0100

    Asma partie

--- projet-gtk-c-tp.c
+++ projet-gtk-c-tp.c
@@ -77,3 +77,56 @@ static void on_dequeue_clicked(GtkButton *button, gpointer user_data) {
 
 //--------------------------------------------------------------------------------------------------------------------
 
+int main(int argc, char **argv) {
+    gtk_init(&argc, &argv);
+    srand((unsigned) time(NULL)); // Initialize random number generator
+
+    // Create a new window
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_window_set_title(GTK_WINDOW(window), "Queue Example");
+    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+
+    // Initialize widgets structure
+    QueueWidgets widgets;
+    widgets.queue = g_queue_new();
+    widgets.dequeued_items = g_string_new(NULL);
+
+    initialize_queue_with_fixed_values(widgets.queue);
+
+    // Vertical box to arrange widgets vertically
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    // Initialize and set up text view
+    widgets.text_view = gtk_text_view_new();
+    gtk_text_view_set_editable(GTK_TEXT_VIEW(widgets.text_view), FALSE);
+    gtk_widget_set_margin_bottom(GTK_WIDGET(widgets.text_view), 5);
+    gtk_box_pack_start(GTK_BOX(vbox), widgets.text_view, TRUE, TRUE, 1);
+
+    // Create and configure the dequeue button
+    GtkWidget *dequeue_button = gtk_button_new_with_label("Dequeue");
+    g_signal_connect(dequeue_button, "clicked", G_CALLBACK(on_dequeue_clicked), &widgets);
+    gtk_widget_set_margin_bottom(GTK_WIDGET(dequeue_button), 5);
+    gtk_box_pack_start(GTK_BOX(vbox), dequeue_button, TRUE, TRUE, 0);
+
+    // Create and configure the enqueue button
+    GtkWidget *enqueue_button = gtk_button_new_with_label("Enqueue");
+    g_signal_connect(enqueue_button, "clicked", G_CALLBACK(on_enqueue_clicked), &widgets);
+    gtk_widget_set_margin_bottom(GTK_WIDGET(enqueue_button), 5);
+    gtk_box_pack_start(GTK_BOX(vbox), enqueue_button, TRUE, TRUE, 0);
+
+    update_text_view(&widgets); // Display initial queue contents
+
+    gtk_widget_show_all(window); // Show all widgets in the window
+
+    gtk_main(); // Main loop of the GTK application
+
+    // Cleanup
+    if (widgets.dequeued_items) {
+        g_string_free(widgets.dequeued_items, TRUE);
+    }
+    g_queue_free_full(widgets.queue, free); // Free the queue and its contents
+
+    return 0;
+}
